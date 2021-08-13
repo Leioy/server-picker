88bb03f6c7a954dc00e2149593898af477238a41
Leioy <Leioy9312@gmail.com>
2021-08-13 15:14:03 +0800

    initial commit

---
+++ examples/calendar.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Moment } from 'moment';
+import Picker from '../src/Picker';
+import PickerPanel from '../src/PickerPanel';
+import momentGenerateConfig from '../src/generate/moment';
+import zhCN from '../src/locale/zh_CN';
+import '../assets/index.less';
+import './calendar.less';
+
+function dateRender(date: Moment, today: Moment) {
+  return (
+    <div
+      style={{
+        width: 80,
+        height: 80,
+        borderTop: '3px solid #CCC',
+        borderTopColor: date.isSame(today, 'date') ? 'blue' : '#CCC',
+      }}
+    >
+      {date.date()}
+    </div>
+  );
+}
+
+const disabledProps = {
+  disabledDate: (date) => date.date() === 10,
+  onSelect: (d) => console.log('Select:', d.format('YYYY-MM-DD')),
+  onChange: (d) => console.log('Change:', d.format('YYYY-MM-DD')),
+};
+
+export default () => (
+  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+    <div>
+      <PickerPanel<Moment>
+        locale={zhCN}
+        // picker="month"
+        generateConfig={momentGenerateConfig}
+        dateRender={dateRender}
+        {...disabledProps}
+      />
+    </div>
+    <div>
+      <Picker<Moment>
+        locale={zhCN}
+        generateConfig={momentGenerateConfig}
+        dateRender={dateRender}
+        {...disabledProps}
+      />
+    </div>
+  </div>
+);
